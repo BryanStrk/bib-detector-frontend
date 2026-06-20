677a8516b38a925d4ac680b6bd55791786451e7d
BryanStrk <126487933+BryanStrk@users.noreply.github.com>
2026-06-20 21:41:18 +0200

    feat: add admin and delete icons

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -126,3 +126,21 @@ export function ChevronDownIcon(props) {
     </svg>
   );
 }
+
+export function TrashIcon(props) {
+  return (
+    <svg {...base} {...props}>
+      <path d="M3 6h18M8 6V4a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2m2 0v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
+      <path d="M10 11v6M14 11v6" />
+    </svg>
+  );
+}
+
+export function LockIcon(props) {
+  return (
+    <svg {...base} {...props}>
+      <rect x="4" y="11" width="16" height="10" rx="2" />
+      <path d="M8 11V7a4 4 0 0 1 8 0v4" />
+    </svg>
+  );
+}
